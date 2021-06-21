0b5ad636e6a78e0ba555c802252525d46cb6131c
Daichi Murokami <daichi@murokami.com>
2021-06-21 04:14:01 -0700

    armies eat food

--- armies/army_base.gd
+++ armies/army_base.gd
@@ -2,11 +2,18 @@ class_name ArmyBase
 extends KinematicBody2D
 
 export var population : int = 100
+export var grain : float = 1000
+export var consumption_rate : float = 0.3 # grain per person per second
 
 onready var select_area = $SelectionArea
 onready var level = get_node("/root/LevelManager").current_level
 onready var hud_controller = level.hud_controller
 onready var map_manager = level.map_manager
+
+# temp hud elements
+onready var food_label = $VBoxContainer/FoodLabel
+onready var pop_label = $VBoxContainer/PopLabel
+onready var starve_label = $VBoxContainer/StarveLabel
 
 # assigned on birth by army manager
 var army_id : int
@@ -17,12 +24,17 @@ var velocity = 100 # sec
 var target : Vector2 = global_position
 var position_error = 5 # pixels from target
 
+# hunger
+
+var starving = false
+
 func _ready():
 	select_area.connect("input_event", self, "on_select_input_event")
 
 func _physics_process(delta):
 	if !map_manager.is_paused:
 		move(delta)
+		consume_grain(delta)
 
 func move(delta):
 	var facing = target - global_position
@@ -30,6 +42,18 @@ func move(delta):
 	if facing.length_squared() < position_error * position_error: return
 	var movement = facing.normalized() * velocity * delta
 	move_and_collide(movement)
+
+func consume_grain(delta):
+	var amount = population * consumption_rate * delta
+	grain -= amount
+	if grain <= 0: 
+		grain = 0
+		starving = true
+		starve_label.visible = true
+	else: 
+		starve_label.visible = false
+	food_label.text = "Food: " + String(int(grain))
+	pop_label.text = "Men: " + String(population)
 
 func on_select_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("ui_lmb"):
