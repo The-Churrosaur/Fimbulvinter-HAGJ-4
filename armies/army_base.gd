8840c2d4fa9ce14dd3ddbdeb0c676e4608e835db
Daichi Murokami <daichi@murokami.com>
2021-06-21 23:48:32 -0700

    armies now contain units, manager tracks units, population by units, each unit has a ui node which is updated by it and can be stolen

--- armies/army_base.gd
+++ armies/army_base.gd
@@ -1,7 +1,6 @@
 class_name ArmyBase
 extends KinematicBody2D
 
-export var population : int = 100
 export var grain : float = 10
 export var consumption_rate : float = 0.1 # grain per person per second
 export var starve_chance : float = 0.01 # chance of starvation death per tick
@@ -12,12 +11,16 @@ onready var hud_controller = level.hud_controller
 onready var map_manager = level.map_manager
 
 # temp hud elements
-onready var food_label = $VBoxContainer/FoodLabel
-onready var pop_label = $VBoxContainer/PopLabel
-onready var starve_label = $VBoxContainer/StarveLabel
+onready var food_label = $UI/VBoxContainer/FoodLabel
+onready var pop_label = $UI/VBoxContainer/PopLabel
+onready var starve_label = $UI/VBoxContainer/StarveLabel
+onready var units_vbox = $UI/VBoxContainer/Units
 
 # assigned on birth by army manager
 var army_id : int
+
+# dictionary of embedded units
+var units = {}
 
 # map movement
 
@@ -46,7 +49,7 @@ func move(delta):
 	move_and_collide(movement)
 
 func consume_grain(delta):
-	var amount = population * consumption_rate * delta
+	var amount = get_manpower() * consumption_rate * delta
 	grain -= amount
 	if grain <= 0: 
 		grain = 0
@@ -55,16 +58,39 @@ func consume_grain(delta):
 	else: 
 		starve_label.visible = false
 	food_label.text = "Food: " + String(int(grain))
-	pop_label.text = "Men: " + String(population)
+	pop_label.text = "Men: " + String(get_manpower())
 
 func starve(delta):
 	if randf() <= starve_chance:
-		population -= 1
+		var unit = units.values()[randi() % units.size()]
+		unit.add_men(-1)
+
+# input ==========
 
 func on_select_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("ui_lmb"):
 		hud_controller.on_army_selected(self)
 
+# movement ===========
+
 func set_target(target):
 	self.target = target
 	print(name, " target set: ", self.target)
+
+# unit management ==========
+
+func get_manpower() -> int:
+	var men = 0
+	for unit in units.values():
+		men += unit.manpower
+	return men
+
+func add_unit(unit):
+	print("unit: ", unit, ", ", unit.unit_id, " added to army: ", name)
+	units[unit.unit_id] = unit
+	
+	# reparent unit ui
+	var ui = unit.unit_ui
+	if ui.get_parent():
+		ui.get_parent().remove_child(ui)
+	units_vbox.add_child(ui)
---
+++ armies/unit_base.gd
@@ -0,0 +1,25 @@
+class_name UnitBase
+extends Node2D
+
+export var manpower = 50
+
+# unit ui exists to be plucked and replaced by armies, tooltips
+onready var unit_ui = $UnitInfo
+onready var unit_label = $UnitInfo/Label
+
+var unit_id = 0
+
+signal manpower_changed(new_manpower)
+
+func _ready():
+	update_label()
+
+func add_men(men):
+	manpower += men
+	if manpower <= 0:
+		manpower = 0
+	emit_signal("manpower_changed", manpower)
+	update_label()
+
+func update_label():
+	unit_label.text = name + ": " + String(manpower) + " men"
